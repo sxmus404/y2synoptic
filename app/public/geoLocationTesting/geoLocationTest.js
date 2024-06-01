6d0fa577d4fe71d51501a46f389ab261cdb51125
AlexFoster10 <wookie471@outlook.com>
2024-06-01 16:18:42 +0100

    Now Brings up Map in test.html

--- app/public/geoLocationTesting/geoLocationTest.js
+++ app/public/geoLocationTesting/geoLocationTest.js
@@ -4,10 +4,25 @@ function showPosition() {
       navigator.geolocation.getCurrentPosition(function(position) {
           var positionInfo = "Your current position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
           document.getElementById("result").innerHTML = positionInfo;
+          var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 17);
+          L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 50,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+
       });
+
+
+
+
   } else {
       alert("Sorry, your browser does not support HTML5 geolocation.");
   }
 }
 
+
 showPosition();
+
+
+
+initMap();
