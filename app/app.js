2821c189ee7e8d50650ad4134538182cb6393d3d
zxckv <zack.langley@icloud.com>
2024-06-01 17:09:04 +0100

    Fixed pathing causing css to not be served

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 const port = 3000;
 
-const favicon = require("serve-favicon");
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+console.log("DIRNAME: " + __dirname);
+app.use(express.static(path.join(__dirname + '/public')));
 
 // Default route to home page
 app.get('/', function(req, res) {
@@ -12,15 +14,9 @@ app.get('/', function(req, res) {
   });
 });
 
+// Route to second page [NEEDS TO BE RENAMED]
 app.get('/cheese', function(req, res) {
   res.sendFile('public/page2.html', {root: __dirname}, (err) => {
-      if (err) {console.log(err);}
-  });
-});
-
-// TESTING ONLY - REMOVE 
-app.get('/ico', function(req, res) {
-  res.sendFile('public/images/favicon.ico', {root: __dirname}, (err) => {
       if (err) {console.log(err);}
   });
 });
