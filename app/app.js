0717659c24293ccb5401a9c5fceb69e98eb2c0e2
AlexFoster10 <wookie471@outlook.com>
2024-06-03 23:07:18 +0100

    Fix

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const path = require ("path");
 const port = 3000;
+app.use(express.static(path.join(__dirname + '/public')));
 
 const favicon = require("serve-favicon");
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
