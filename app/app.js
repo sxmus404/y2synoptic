4ae036a9adb457bbf39ebc03f3063c0519303bb7
Jay Wharton <jay.wharton@sky.com>
2024-06-03 16:59:38 +0100

    Connected database

--- app/app.js
+++ app/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
+const { Client } = require('pg')
 
 const app = express();
-const port = 3000;
+const port = 3000; 
 
 console.log("DIRNAME: " + __dirname);
 app.use(express.static(path.join(__dirname + '/public')));
@@ -38,4 +39,22 @@ app.get('/about', function(req, res) {
 app.listen(port, ()=> {
   console.log('Server Running');
   console.log('http://localhost:3000/');
+});
+
+const client = new Client({
+  user: 'postgres',
+  host: '81.99.224.111',
+  database: 'postgres',      //Connect to Sam's server
+  password: 'zackisgay',
+  port: 5432,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+client.query("SELECT * FROM crop_info", function (err, result){
+  if (err) throw err;                                         //Generalised code on how to make a query
+  console.log(result.rows[1].cropid);                         //important you do result.rows and not just result so that it doesnt print all table info
+                                                              //to get specific value do "." and then whatever its called in the table(as shown)
 });
