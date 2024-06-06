f05377d85cef7938ed8dcfc73f9c11423681cbbd
Jay Wharton <jay.wharton@sky.com>
2024-06-06 16:58:33 +0100

    removed database stuff from app

--- app/app.js
+++ app/app.js
@@ -40,21 +40,3 @@ app.listen(port, ()=> {
   	console.log('http://localhost:3000/');
 });
 
-const client = new Client({
-	user: 'postgres',
-	host: '81.99.224.111',
-	database: 'postgres', //Connect to Sam's server
-	password: 'zackisgay',
-	port: 5432,
-})
-
-client.connect(function(err) {
-	if (err) {throw err;}
-	console.log("Connected!");
-});
-
-// client.query("SELECT * FROM crop_info", function (err, result){
-//   if (err) throw err;                                         //Generalised code on how to make a query
-//   console.log(result.rows[1].cropid);                         //important you do result.rows and not just result so that it doesnt print all table info
-//                                                               //to get specific value do "." and then whatever its called in the table(as shown)
-// });
