6df86e9c669d1ac289c030550dfab7f646d2e173
Sxmus <101070122+sxmus404@users.noreply.github.com>
2024-06-09 00:47:44 +0100

    Update app.js

--- app/app.js
+++ app/app.js
@@ -42,3 +42,43 @@ app.listen(port, ()=> {
   	console.log('Server Running');
   	console.log('http://localhost:3000/');
 });
+
+
+<<<<<<< translator-branch
+app.post('/translate', async (req, res) => {
+	const {text, language} = req.body;
+	try {
+		const result = await googleTranslate.translate(text, {to:language});
+		res.json({ translation: result.text });
+	} catch (error) {
+		console.error("Translation error: ", error.message);
+		res.status(500).json({error:error.message});
+	}
+});
+
+// async function waiting() {
+// 	const result = await googleTranslate.translate('Hello, how are you?', { to: 'km' });
+// 	console.log(result.text) // => 'Hello World! How are you?'
+// }
+// waiting()
+
+const client = new Client({
+	user: 'postgres',
+	host: '81.99.224.111',
+	database: 'postgres', //Connect to Sam's server
+	password: 'zackisgay',
+	port: 5432,
+})
+
+client.connect(function(err) {
+	if (err) {throw err;}
+	console.log("Connected!");
+});
+
+// client.query("SELECT * FROM crop_info", function (err, result){
+//   if (err) throw err;                                         //Generalised code on how to make a query
+//   console.log(result.rows[1].cropid);                         //important you do result.rows and not just result so that it doesnt print all table info
+//                                                               //to get specific value do "." and then whatever its called in the table(as shown)
+// });
+=======
+>>>>>>> main
