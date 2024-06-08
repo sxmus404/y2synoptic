dee6e9d19d18a386e5133ffca1f4c474b5a69770
GreenMan891 <greenman891@hotmail.com>
2024-06-08 13:05:03 +0100

    added the toggle slider

--- app/public/translator.js
+++ app/public/translator.js
@@ -1,26 +1,38 @@
+var currentLanguage = "en"
 document.addEventListener('DOMContentLoaded', () => {
-    const texts = document.querySelectorAll('.translate');
-    const language = "km";
-    if (language == "en") {
-
-    } else {
-        texts.forEach(elementText => {
-            const content = elementText.innerText;
-    
-            fetch('/translate', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({text: content, language})
-            })
-            .then(response => response.json())
-            .then(data => {
-                elementText.innerText = data.translation;
-            })
-            .catch(error => {
-                console.error("Error: ", error)
-            })
-        });
-    }
+    const checkbox = document.getElementById("dark")
+    checkbox.addEventListener("change", (event) => {
+        if (event.target.checked) {
+            translate("km")
+        } else {
+            translate("en")
+        }
+    })
 });
+
+function translate(language) {
+    console.log("why are we getting")
+    const texts = document.querySelectorAll('.translate');
+    if (currentLanguage == "en") {
+        if (language == "km") {
+            texts.forEach(elementText => {
+                const content = elementText.innerText;
+    
+                fetch('/translate', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ text: content, language })
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        elementText.innerText = data.translation;
+                    })
+                    .catch(error => {
+                        console.error("Error: ", error)
+                    })
+            }); 
+        }
+    } 
+}
