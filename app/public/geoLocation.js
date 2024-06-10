83e82ef15cfc9d38d908045e0a0a819e15872141
BlexDex <mkf22mvu@uea.ac.uk>
2024-06-10 21:18:18 +0100

    CUM

--- app/public/geoLocation.js
+++ app/public/geoLocation.js
@@ -1,12 +1,14 @@
 //33ab05142e62d332c43e4a6bd89e6df3 weather API key
+var map;
+var lat_;
+var lon_;
 document.addEventListener("DOMContentLoaded", (event) => {
-	showPosition();
 	
   });
 function showPosition() {
 
-	var lat_;
-	var lon_;
+	lat_;
+	lon_;
 	if(navigator.geolocation) {
 		var container = L.DomUtil.get('map-main');
 		if(container != null){ container._leaflet_id = null; }
@@ -16,12 +18,16 @@ function showPosition() {
 			lon_ = position.coords.longitude;
 			var positionInfo = "Your current position is (" + "Latitude: " + lat_ + ", " + "Longitude: " + lon_ + ")";
 			document.getElementById("map-result").innerHTML = positionInfo;
-			var map = L.map('map-main').setView([position.coords.latitude, position.coords.longitude], 19);
+			map = L.map('map-main').setView([position.coords.latitude, position.coords.longitude], 19);
 			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 				maxZoom: 19,
 				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 			}).addTo(map);
 			var marker = L.marker([lat_, lon_]).addTo(map);
+			var popup = L.popup()
+    			.setLatLng([lat_, lon_])
+    			.setContent("I am a standalone popup.")
+    			.openOn(map);
 			map.on('click', onMapClick);
 			console.log(lat_ + "//" + lon_);
 			fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat_+'&lon='+lon_+'&appid=93b803fddcaf9fac244d7f72437b87f7') //API KEY ERROR, WILL WAIT AS MIGHT BE USING TO OFTEN
@@ -37,12 +43,10 @@ function showPosition() {
 		;
 	} else { alert("Sorry, your browser does not support HTML5 geolocation."); }
 }
-var popup = L.popup();
 function onMapClick(e) {
+	alert(e.latlng)
     popup
-        .setLatLng(e.latlng)
+        .setLatLng(lat_, lon_)
         .setContent("You clicked the map at " + e.latlng.toString())
         .openOn(map);
 }
-map.on('click', onMapClick);
-showPosition();
