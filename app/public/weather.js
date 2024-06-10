f6f7d61fd4a256c9867b2ba92ae67721243d5465
zxckv <zack.langley@icloud.com>
2024-06-10 16:38:25 +0100

    Minor reformatting

--- app/public/weather.js
+++ app/public/weather.js
@@ -1,9 +1,10 @@
 function setupWeatherWidget() {
     window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
-    window.windowID = 11;
+    window.windowID = 15;
     window.cityID = idValue;
     window.appID = '93b803fddcaf9fac244d7f72437b87f7';
-    window.containerID = 'openweathermap-widget-11';
+    window.containerID = 'openweathermap-widget-15';
+
     window.myWidgetParam.push({
         id: windowID,
         cityid: cityID,
@@ -11,9 +12,12 @@ function setupWeatherWidget() {
         units: 'metric',
         containerid: containerID
     });  
+
     (function() {
-        var script = document.createElement('script');script.async = true;script.charset = "utf-8";
+        var script = document.createElement('script'); 
+        script.async = true;
         script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
-        var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);
-      })();    
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(script, s);
+    })();    
 }
