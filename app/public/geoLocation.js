ee8875d2789def702d948b1e5155b80ab4f10ab6
BlexDex <mkf22mvu@uea.ac.uk>
2024-06-10 20:47:50 +0100

    Started work on the field markers

--- app/public/geoLocation.js
+++ app/public/geoLocation.js
@@ -1,4 +1,8 @@
 //33ab05142e62d332c43e4a6bd89e6df3 weather API key
+document.addEventListener("DOMContentLoaded", (event) => {
+	showPosition();
+	
+  });
 function showPosition() {
 
 	var lat_;
@@ -14,9 +18,11 @@ function showPosition() {
 			document.getElementById("map-result").innerHTML = positionInfo;
 			var map = L.map('map-main').setView([position.coords.latitude, position.coords.longitude], 19);
 			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-				maxZoom: 50,
+				maxZoom: 19,
 				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 			}).addTo(map);
+			var marker = L.marker([lat_, lon_]).addTo(map);
+			map.on('click', onMapClick);
 			console.log(lat_ + "//" + lon_);
 			fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat_+'&lon='+lon_+'&appid=93b803fddcaf9fac244d7f72437b87f7') //API KEY ERROR, WILL WAIT AS MIGHT BE USING TO OFTEN
 			.then(response => response.json())
@@ -32,4 +38,8 @@ function showPosition() {
 	} else { alert("Sorry, your browser does not support HTML5 geolocation."); }
 }
 
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+map.on('click', onMapClick);
 showPosition();
