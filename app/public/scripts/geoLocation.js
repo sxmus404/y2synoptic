9ac82de6951c32d87279917ccfc5951990504247
BlexDex <mkf22mvu@uea.ac.uk>
2024-06-10 23:34:43 +0100

    markers and such

--- app/public/scripts/geoLocation.js
+++ app/public/scripts/geoLocation.js
@@ -2,35 +2,55 @@
 var map;
 var lat_;
 var lon_;
-let idValue = '54'
-let currentWeather = "n/a"
+var popup;
+let idValue;
+let currentWeather;
+const fieldLat = [12.5898727551881,12.589786370543436,12.58969736815183,12.589699985869677,12.589621454322831,
+	12.589587423978399,12.58953506959354,12.589592659416292,12.58975495793816,12.589857048891245,12.590021964960393,
+	12.590050759818737,12.590228764325854,12.590377973891067,12.590268030009332,12.590134526661004,12.589987934669088,
+	12.589891079199903,12.589839251634079,12.589476953102275,12.589587423978399,12.589741869351837,12.590032435818703,
+	12.590160703793956,12.590273265433693]
+const fieldLon = [106.92497569790845,106.92496228686365, 106.92494082919198, 106.92481476537091, 106.9247638034007, 
+	106.92494887581887, 106.92477989665444, 106.92458945981836, 106.92460555307213, 106.92475843898278, 106.92489523163968
+	, 106.92477185002757, 106.92483622304258, 106.9247691678186, 106.92465919725129, 106.9246189641169, 106.92459482423628
+	, 106.92447948925104, 106.92460823528107, 106.92458141319148, 106.92440706960916, 106.92441779844499, 106.92443120948978
+	, 106.92445803157938, 106.9244633959973]
+	
 document.addEventListener("DOMContentLoaded", (event) => {
 	showPosition();
   });
+
 function showPosition() {
-	var lat_;
-	var lon_;
 	if(navigator.geolocation) {
 		var container = L.DomUtil.get('map-main');
 		if(container != null){ container._leaflet_id = null; }
-
+		// Map
       	navigator.geolocation.getCurrentPosition( function(position) {
-			lat_ = position.coords.latitude;
-			lon_ = position.coords.longitude;
+			// current location
+			// lat_ = position.coords.latitude;
+			// lon_ = position.coords.longitude;
+
+			// Pu Ngaol
+			lat_ = 12.58973689927903;
+			lon_ = 106.925101118714666;
 			var positionInfo = "Your current position is (" + "Latitude: " + lat_ + ", " + "Longitude: " + lon_ + ")";
 			document.getElementById("map-result").innerHTML = positionInfo;
 			map = L.map('map-main').setView([position.coords.latitude, position.coords.longitude], 19);
 			L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-				maxZoom: 19,
+				maxZoom: 50,
 				attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 			}).addTo(map);
-			var marker = L.marker([lat_, lon_]).addTo(map);
-			var popup = L.popup()
-    			.setLatLng([lat_, lon_])
-    			.setContent("I am a standalone popup.")
-    			.openOn(map);
+			var currentLocation = L.marker([lat_, lon_]).addTo(map).bindPopup("You Are Here").openPopup();
+
+			for (let i = 0; i < fieldLat.length; i++) {
+				L.marker([fieldLat[i],fieldLon[i]]).addTo(map).bindPopup((i + 1).toString())
+			  }
+
 			map.on('click', onMapClick);
+		
 			console.log(lat_ + "//" + lon_);
+			
+			// Weather
 			fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat_+'&lon='+lon_+'&appid=93b803fddcaf9fac244d7f72437b87f7') //API KEY ERROR, WILL WAIT AS MIGHT BE USING TO OFTEN
 			.then(response => response.json())
 			.then(data => { 
@@ -49,10 +69,12 @@ function showPosition() {
 		;
 	} else { alert("Sorry, your browser does not support HTML5 geolocation."); }
 
+}
 function onMapClick(e) {
-	alert(e.latlng)
+	console.log(e.latlng)
+	popup = L.popup();
     popup
-        .setLatLng(lat_, lon_)
-        .setContent("You clicked the map at " + e.latlng.toString())
+        .setLatLng(e.latlng)
+        .setContent("Would you like to add a field?")
         .openOn(map);
-}}
+}
