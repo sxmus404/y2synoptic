b6b91730e18e8540c6bb104ac1919e7425534b37
zxckv <zack.langley@icloud.com>
2024-06-11 01:07:56 +0100

    Switch weather widget

--- app/public/scripts/weather.js
+++ app/public/scripts/weather.js
@@ -1,9 +1,9 @@
 function setupWeatherWidget() {
     window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
-    window.windowID = 15;
+    window.windowID = 11;
     window.cityID = idValue;
     window.appID = '93b803fddcaf9fac244d7f72437b87f7';
-    window.containerID = 'openweathermap-widget-15';
+    window.containerID = 'openweathermap-widget-11';
 
     window.myWidgetParam.push({
         id: windowID,
@@ -19,5 +19,5 @@ function setupWeatherWidget() {
         script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(script, s);
-    })();    
+    })();
 }
