ab331c494f6d3db50fa2bb2199527e9b0c448da1
zxckv <zack.langley@icloud.com>
2024-06-11 21:47:54 +0100

    Added queries to POST requests

--- app/app.js
+++ app/app.js
@@ -18,6 +18,14 @@ const client = new Client({
 	port: 5432
 });
 
+// Function for connection and query of the database
+async function pgConnect(queryString) {
+	await client.connect();
+	try { return await client.query(queryString); } 
+	catch (err) { console.error(err); } 
+	finally { await client.end(); }
+};
+
 // Default route to home page
 app.get('/', function(req, res) {
   	res.sendFile('public/home.html', {root: __dirname}, (err) => {
@@ -57,9 +65,53 @@ app.post('/translate', async(req, res) => {
 	}
 });
 
-app.post('/query', async(req, res) => {
-	pgConnect().then(data => {
+app.post('/query/addCrop', async(req, res) => {
+    pgConnect("INSERT INTO crop_info (" + req.body.cropType + ", " + req.body.avgGrowthTime + ", " + req.body.irrCycle + ") VALUES (" + req.body.cropType + ", " + req.body.avgGrowthTime + ", " + req.body.irrCycle + ")").then(data => {
+		if (err) { throw err; }
+	});
+});
+
+app.post('/query/getCrop', async(req, res) => {
+    pgConnect("SELECT * FROM crop_info").then(data => {
+		if (err) { throw err; }
 		res.send(data);
+	});
+});
+
+app.post('/query/getField', async(req, res) => {
+    condition = false;
+    array = [];
+
+    for (i = 0; !condition; i++) {
+        pgConnect("Select estHarvest From field_info WHERE fieldnum = " + i).then(data => {
+			if (err) { throw err; }
+			if (data == NULL) { condition == true; }
+			else { array.push(data); }
+		});
+        num++;
+    };
+
+	res.send(array);
+});
+
+app.post('/query/getDate', async(req, res) => {
+    pgConnect("Select estHarvest From field_info WHERE estHarvest = " + req.body.date).then(data => {
+		if (err) { throw err; } 
+		res.send(data);
+	});
+});
+
+app.post('/query/addField', async(req, res) => {
+    pgConnect("INSERT INTO field_info (fieldNum, cropType, datePlanted, fieldOwner) VALUES (" + req.body.fieldNum + ", " + req.body.cropType + ", " + req.body.datePlanted, ", " + req.body.fieldOwner + ")").then(data => {
+		if (err) { throw err; }
+	})
+
+   	pgConnect("INSERT INTO crop_info (irrCycle) VALUES (SELECT irrCycle FROM crop_info WHERE cropType = " + req.body.cropType + ")").then(data => {
+		if (err) { throw err; }
+	});
+
+    pgConnect("UPDATE field_info SET estHarvest = ((SELECT datePlanted FROM field_info WHERE fieldNum = " + req.body.fieldNum + ") + (SELECT avgGrowthTime FROM crop_info WHERE cropType = " + req.body.cropType + ")) WHERE fieldNum = " + req.body.fieldNum).then(data => {
+		if (err) { throw err; }
 	});
 });
 
@@ -67,16 +119,3 @@ app.listen(port, ()=> {
   	console.log('Server Running');
   	console.log('http://localhost:3000/');
 });
-
-async function pgConnect() {
-	await client.connect();
-	try { return await client.query('SELECT * FROM crop_info');
-	} catch (err) { console.error(err);
-	} finally { await client.end() }
-}
-
-// function getCrops() { 
-// 	return new Promise((resolve, reject) => {
-// 		client.query("SELECT * FROM crop_info");
-// 	});
-// };
