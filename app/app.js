5cf81843c0dd8a7ccce0b635637297b105186075
zxckv <zack.langley@icloud.com>
2024-06-12 18:03:54 +0100

    Updated query to function as intended

--- app/app.js
+++ app/app.js
@@ -102,7 +102,7 @@ app.post('/query/getDate', async(req, res) => {
 
 app.post('/query/addField', async(req, res) => {
 	var queryString1 = ("INSERT INTO field_info (fieldNum, cropType, datePlanted, fieldOwner) VALUES (" + req.body.fieldNum + ", \'" + req.body.cropType + "\', \'" + req.body.datePlanted + "\', \'" + req.body.fieldOwner + "\')");
-    var queryString2 = ("INSERT INTO field_info (irrCycle) VALUES (SELECT irrCycle FROM crop_info WHERE cropType = \'" + req.body.cropType + "\')");
+    var queryString2 = ("UPDATE field_info SET irrcycle = (SELECT irrcycle FROM crop_info WHERE croptype = \'" + req.body.cropType + "\') where fieldnum = " + req.body.fieldNum);
 	var queryString3 = ("UPDATE field_info SET estHarvest = ((SELECT datePlanted FROM field_info WHERE fieldNum = " + req.body.fieldNum + ") + (SELECT avgGrowthTime FROM crop_info WHERE cropType = \'" + req.body.cropType + "\')) WHERE fieldNum = " + req.body.fieldNum);
 	
 	await client.query(queryString1);
