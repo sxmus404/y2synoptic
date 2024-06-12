57217226512376d05c436d24ebd45518156afabc
zxckv <zack.langley@icloud.com>
2024-06-12 17:35:28 +0100

    Restructured query

--- app/public/scripts/queries.js
+++ app/public/scripts/queries.js
@@ -1,122 +1,60 @@
-const { Client } = require('pg');
-const client = new Client({
+// const addCrop = {
+//     cropType: "",
+//     avgGrowthTime: "",
+//     irrCycle: ""
 
-    user: 'postgres',
+// };
 
-    host: '81.99.224.111',
+// const addField = {
+//     fieldNum: "",
+//     cropType: "",
+//     datePlanted: "",
+//     fieldOwner: ""
+// };
 
-    database: 'postgres', //Connect to Sam's server
+// // Add a new crop
+// fetch('/query/addCrop', {
+//     method: 'POST',
+//     body: JSON.stringify(addCrop)
+// }).then(response => response.json()).then(data => {
+//    // WHAT TO DO WITH THE DATA RETURNED HERE
+// }).catch(error => {
+//     console.error("Error: ", error)
+// });
 
-    password: 'zackisgay',
+// // Add a new field
+// fetch('/query/addField', {
+//     method: 'POST',
+//     body: JSON.stringify(addField)
+// }).then(response => response.json()).then(data => {
+//    // WHAT TO DO WITH THE DATA RETURNED HERE
+// }).catch(error => {
+//     console.error("Error: ", error)
+// });
 
-    port: 5432,
-
-  })
-
-  
-
-  client.connect(function(err) {
-
-    if (err) throw err;
-
-    console.log("Connected!");
-
-  });
-
-  
-
-  client.query("SELECT * FROM crop_info", function (err, result){
-
-    if (err) throw err;                                         //Generalised code on how to make a query
-
-    console.log(result.rows[1].cropid);                         //important you do result.rows and not just result so that it doesnt print all table info
-
-                                                                //to get specific value do "." and then whatever its called in the table(as shown)
-
-  });
-
-
-
-
-
-
-function createField(fieldNum,cropType,datePlanted,fieldOwner){
-
-    client.query("INSERT INTO field_info (fieldNum, cropType, datePlanted, fieldOwner) VALUES (",fieldNum,", ",cropType,", ",datePlanted,", ",fieldOwner,")", function(err,result){
-
-        if(err) throw err;
-
-});
-
-    client.query("INSERT INTO crop_info (irrCycle) VALUES ((SELECT irrCycle FROM crop_info WHERE cropType = ,",cropType,"))", function(err,result){
-
-        if(err) throw err;
-
-    });
-
-    client.query("UPDATE field_info SET estHarvest = ((SELECT datePlanted FROM field_info WHERE fieldNum = ",fieldNum,") + (SELECT avgGrowthTime FROM crop_info WHERE cropType = ",cropType,")) WHERE fieldNum = ",fieldNum, function(err,result){
-
-        if(err) throw err;
-
-    });
-
-const addCrop = {
-    cropType: "",
-    avgGrowthTime: "",
-    irrCycle: ""
-
-};
-
-const addField = {
-    fieldNum: "",
-    cropType: "",
-    datePlanted: "",
-    fieldOwner: ""
-};
-
-// Add a new crop
-fetch('/query/addCrop', {
-    method: 'POST',
-    body: JSON.stringify(addCrop)
-}).then(response => response.json()).then(data => {
-   // WHAT TO DO WITH THE DATA RETURNED HERE
-}).catch(error => {
-    console.error("Error: ", error)
-});
-
-// Add a new field
-fetch('/query/addField', {
-    method: 'POST',
-    body: JSON.stringify(addField)
-}).then(response => response.json()).then(data => {
-   // WHAT TO DO WITH THE DATA RETURNED HERE
-}).catch(error => {
-    console.error("Error: ", error)
-});
-
-// Gets all the information on all crops
-fetch('/query/getCrop', {
-    method: 'POST'
-}).then(response => response.json()).then(data => {
-   // WHAT TO DO WITH THE DATA RETURNED HERE
-}).catch(error => {
-    console.error("Error: ", error)
-});
-}
-return array;
+// // Gets all the information on all crops
+// fetch('/query/getCrop', {
+//     method: 'POST'
+// }).then(response => response.json()).then(data => {
+//    // WHAT TO DO WITH THE DATA RETURNED HERE
+// }).catch(error => {
+//     console.error("Error: ", error)
+// });
 
 
 
-// Gets all the information on all fields
-fetch('/query/getFields', {
-    method: 'POST'
-}).then(response => response.json()).then(data => {
-   // WHAT TO DO WITH THE DATA RETURNED HERE
-}).catch(error => {
-    console.error("Error: ", error)
-});
+// // Gets all the information on all fields
+// fetch('/query/getFields', {
+//     method: 'POST'
+// }).then(response => response.json()).then(data => {
+//    // WHAT TO DO WITH THE DATA RETURNED HERE
+// }).catch(error => {
+//     console.error("Error: ", error)
+// });
 
-function addFieldTemp(){
+var fieldForm = document.getElementById("field-form");
+
+fieldForm.addEventListener("click", function() {
     e.preventDefault();
     const addField = {
         fieldNum: "21",
@@ -134,6 +72,4 @@ function addFieldTemp(){
     }).catch(error => {
         console.error("Error: ", error)
     });
-}
-
-document.getElementById("field-form").addEventListener("submit", addFieldTemp())
+});
